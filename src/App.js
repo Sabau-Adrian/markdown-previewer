b8da29ea0cf10fa38c14739dad576a5d43e85585
Sabau Adrian <sabau.adrian1995@gmail.com>
2022-04-12 10:59:24 +0300

    final commit

--- src/App.js
+++ src/App.js
@@ -6,46 +6,54 @@ import Editor from './Editor'
 
 function App() {
   const initialState = `
-  This is a paragraph
   
   # Heading
   ## Heading 2
   
   This **word** is bold.
 
+  ## List:
   - list item 1
   - list item 2
   - list item 3
 
-  This is a inline code: \`<p>Hello World!</p>\`
+  ## Inline code: \`<p>Hello World!</p>\`
 
-  This is a blockquote:
+  ## Blockquote:
   > Dorothy followed her through many of the beautiful rooms in her castle.
 
-  this is multi-line code:
+  ## Multi-line code:
   \`\`\`
   const arr1 = [1, 5, 6];
   const arr2 = [7, 8, 9, 9, 9];
-
   const mergeResult = [...arr1, ...arr2];
   \`\`\`
 
   My website is: [Sabau Adrian Website](https://sabauadrian.website).
 
-  This is me:
-  ![Me!](https://sabauadrian.website/static/media/Linkedin.1b2a446586de178a7840.png)
+  ## Image of Markdown Logo
+  ![Markdown](https://cdn0.iconfinder.com/data/icons/octicons/1024/markdown-512.png)
 `
-  // a heading element (H1 size), a sub heading element (H2 size), a link, inline code, a code block, a list item, a blockquote, an image, and bolded text.
+  
   const [content, setContent] = React.useState(initialState)
   const handleTextChange = (event) => {
     setContent(event.target.value)
   }
 
   return (
+    
     <div className="container">
-      <div><Editor content={content} handleTextChange={handleTextChange}/></div>
-      <div id="preview"><ReactMarkdown>{content}</ReactMarkdown></div>
-      
+      <div>
+        <h1>Markdown Previewer</h1>
+      <div className="headers"> 
+        <h2>Editor:</h2>
+        <h2>Preview:</h2>
+      </div>
+      </div>
+      <div className="previewContainer">
+        <div><Editor content={content} handleTextChange={handleTextChange}/></div>
+        <div id="preview"><ReactMarkdown>{content}</ReactMarkdown></div>
+      </div>
     </div>
     
   )
